d90809ca48d84cdd71eda60ee9a977dabb51e0e4
KapadiDhruv <dhruvkapadi.b@gmail.com>
2023-03-19 12:04:02 +0530

    fix : add gitignore file

--- testGallery.py
+++ testGallery.py
@@ -1,18 +1,21 @@
 import os
-# https://github.com/mikf/gallery-dl
-
-# pip3 install gallery-dl
+import subprocess
 
 from_file = 'onlygallery.txt'
 to_file = 'finalgallery.txt'
 
-file = open(from_file,"r+")
+file = open(from_file, "r+")
 gallery = []
 for line in file:
     sub = 'https://www.reddit.com/gallery/' + line.strip()
     print(sub)
     url = 'gallery-dl -g '
     r = url + sub
-    # print(r)
-    os.system(r)
+    # capture output of command
+    result = subprocess.check_output(r, shell=True)
+    # append output to gallery list
+    gallery.append(result.decode())
     
+# write gallery list to finalgallery.txt
+with open(to_file, 'w') as f:
+    f.write('\n'.join(gallery))
